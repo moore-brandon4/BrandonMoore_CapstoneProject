acbffb508bcc7561662905d67f128d1b76a220c4
moore-brandon4 <moore_brandon4@columbusstate.edu>
2021-11-08 17:08:29 +0000

    urls updated for index

--- capstone.py
+++ capstone.py
@@ -8,5 +8,23 @@ capstone = Flask(__name__)
 @capstone.route("/")                          
 def index():                    
     return render_template("index.html")
+
+@capstone.route("/programming")                          
+def programming():                    
+    return render_template("programming.html")
+
+@capstone.route("/webandappdevelopment")                          
+def web():                    
+    return render_template("web.html")  
+
+@capstone.route("/microsoft365")                          
+def microsoft():                    
+    return render_template("microsoft.html")  
+
+@capstone.route("/collaboration")                          
+def collaboration():                    
+    return render_template("collaboration.html")  
+
+
          
 capstone.run(debug=True)                   
